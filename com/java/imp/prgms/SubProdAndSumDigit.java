bca290df647c6d78009217ca0b4f724b0ca11b9d
asingh4305 <asingh.it@hotmail.com>
2020-08-05 21:19:55 +0530

    Easy LeetCode : 1365 & 1281 updated

--- com/java/imp/prgms/SubProdAndSumDigit.java
+++ com/java/imp/prgms/SubProdAndSumDigit.java
@@ -16,20 +16,20 @@ package com.java.imp.prgms;
 public class SubProdAndSumDigit {
 
 	public static void main(String[] args) {
-		
+
 		int nums  = 234;
 		System.out.println(subtractProductAndSum(nums));
 	}
 
     public static int subtractProductAndSum(int n) {
         int sum = 0;
-        int product = 1;
-        
+        int multiply = 1;
+
         while (n > 0){
-            sum = sum + n % 10;
-            product = product * n % 10;
+            sum +=  n % 10;
+            multiply *=  n % 10;
             n = n/10;
         }
-        return product-sum;
+        return multiply - sum;
     }
 }
--- com/java/imp/prgms/XORArray.java
+++ com/java/imp/prgms/XORArray.java
@@ -1,6 +1,14 @@
 /**
 * XOR Operation in an Array
 * LeetCode id : 1486
+* 
+* EXAMPLE:
+* 
+* where nums[i] = start + 2*i (0-indexed) and n == nums.length
+* 
+* Input: n = 5, start = 0
+* Output: 8
+* Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
 **/
 
 package com.java.imp.prgms;
