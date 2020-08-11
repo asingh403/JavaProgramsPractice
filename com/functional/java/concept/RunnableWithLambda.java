593e00857d5eaf8f0d84c8d14e82a69ed224556c
asingh4305 <asingh.it@hotmail.com>
2020-08-11 22:45:10 +0530

    With Lambda Expression Functional Concept

--- com/functional/java/concept/RunnableWithLambda.java
+++ com/functional/java/concept/RunnableWithLambda.java
@@ -1,16 +1,14 @@
 package com.functional.java.concept;
 
-public class RunnableConcepts {
+public class RunnableWithLambda {
 
 	public static void main(String[] args) {
 		Runnable r = () -> {
-
 			int i = 0;
 			while (i++ < 10) {
 				System.out.println("Its works !!");
 			}
 		};
-		new Thread(r).start();
-	}
 
+	}
 }
